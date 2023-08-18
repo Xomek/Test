e017cc56c52cd301cc4b1ea449e2f603ffa5355f
XomekX <z_v_m_x@mail.ru>
2023-08-18 16:32:21 +0700

    create: ButtonControl component template

---
+++ src/components/ButtonControl/ButtonControl.tsx
@@ -0,0 +1,7 @@
+import { ButtonControlProps } from "./ButtonControl.types";
+
+const ButtonControl: React.FC<ButtonControlProps> = () => {
+  return <div></div>;
+};
+
+export default ButtonControl;
