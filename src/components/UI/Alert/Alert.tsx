4d8627fdb5733b9ef6d0661951e0d17addd360cb
XomekX <z_v_m_x@mail.ru>
2023-08-19 13:22:35 +0700

    create: Alert component template

---
+++ src/components/UI/Alert/Alert.tsx
@@ -0,0 +1,5 @@
+const Alert: React.FC = () => {
+  return <div></div>;
+};
+
+export default Alert;
--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,5 +1,6 @@
 import AutocompleteControl from "./AutocompleteControl";
 import TextField from "./TextField";
 import Button from "./Button";
+import Alert from "./Alert";
 
-export { AutocompleteControl, TextField, Button };
+export { AutocompleteControl, TextField, Button, Alert };
